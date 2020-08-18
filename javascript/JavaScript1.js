5c40c37890ebde73fd4cd4b4c61ab7ddcc0a1ee7
Abdullah Hussain <abdnhussain@gmail.com>
2020-08-18 15:05:00 +0100

    This is the JS code for a popup in Dynamics

---
+++ javascript/JavaScript1.js
@@ -0,0 +1,34 @@
+﻿
+function AccountOnLoad() {
+    if (Xrm.Page.getAttribute("mobilephone").getValue() == null && Xrm.Page.getAttribute("emailaddress1").getValue() == null ) {
+        var confirmStrings = { text: "Mobile Phone has less than 10 digits.", title: "Mobile Phone Validation" };
+        var confirmOptions = { height: 200, width: 450 };
+
+        Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
+            function (success) {
+                if (success.confirmed)
+                    console.log("Dialog closed using OK button.");
+                else
+                    console.log("Dialog closed using Cancel button or X.");
+            });
+    }
+}
+
+
+this.onChange_MobilePhone = function () {
+    //var formContext = executionContext.getFormContext();
+    var mobilePhone = formContext.getAttribute("mobilephone").getValue();
+    if (mobilePhone != null && mobilePhone.length < 10) {
+
+        var confirmStrings = { text: "Mobile Phone has less than 10 digits.", title: "Mobile Phone Validation" };
+        var confirmOptions = { height: 200, width: 450 };
+
+        Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
+            function (success) {
+                if (success.confirmed)
+                    console.log("Dialog closed using OK button.");
+                else
+                    console.log("Dialog closed using Cancel button or X.");
+            });
+    }
+}
